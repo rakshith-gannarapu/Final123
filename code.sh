b6f36ccb5dd726fedf1f17227fca37e51c436355
rakshith-gannarapu <59986312+rakshith-gannarapu@users.noreply.github.com>
2020-08-19 23:34:25 -0400

    Update code.sh

--- code.sh
+++ code.sh
@@ -4,6 +4,6 @@ sudo apt-get update
 sudo apt-get -y install ansible
 sudo apt-get -y install git
 git clone https://github.com/rakshith-gannarapu/Final123.git
-cd Final123; sudo ansible-playbook kubernetes.yml
+cd Final123; sudo ansible-playbook kubernetes.yaml
 
 
